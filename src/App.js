68d460121ef3c96d43bbf98ab9d28727e99dc00b
Divya <divyakeerthisravani999@gmail.com>
2020-07-03 13:23:10 +0530

    my react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import pro from './img/profile.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Home/>
     </div>
+  );
+}
+let Home=()=>{
+  return(
+      <div className="row justify-content-center">
+        <div className="card" style={{width:"25rem",fontStyle:"italic",boxShadow:"0px 8px 8px -8px #1000"}}>
+        <img src={pro} style={{height:"50%"}}/>
+        <div className="card-body text-center">
+        <h1>Adabala Divya</h1>
+        </div>
+      </div>
+      </div>
   );
 }
 
