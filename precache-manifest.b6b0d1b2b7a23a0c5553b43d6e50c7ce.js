e01727c45567672560921ce2a681e47a53b48ece
Divya <divyakeerthisravani999@gmail.com>
2020-07-03 12:48:59 +0530

    Updates

---
+++ precache-manifest.b6b0d1b2b7a23a0c5553b43d6e50c7ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd41696dde01b3945c157f6aebe4707",
+    "url": "/my-reactapp/index.html"
+  },
+  {
+    "revision": "eb340beacad771680699",
+    "url": "/my-reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a378e3956d17a8d244c6",
+    "url": "/my-reactapp/static/js/2.a7c7d7b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-reactapp/static/js/2.a7c7d7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb340beacad771680699",
+    "url": "/my-reactapp/static/js/main.48e1498b.chunk.js"
+  },
+  {
+    "revision": "2b95c5c152e6efac5ab4",
+    "url": "/my-reactapp/static/js/runtime-main.89b30ccf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
